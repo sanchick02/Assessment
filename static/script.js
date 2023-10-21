211d137353ae9ea35f2095ffd667d851adafde3b
sanchick02 <115912003+sanchick02@users.noreply.github.com>
2023-10-22 00:03:09 +0800

    Remove commented olf code

--- static/script.js
+++ static/script.js
@@ -1,37 +1,3 @@
-// // script.js
-// function recommendProducts() {
-//     var query = document.getElementById('query').value;
-//     fetch('/recommend_products', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ query: query })
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         const recommendations = document.getElementById('recommendations');
-//         recommendations.innerHTML = '';
-//         data.recommendations.forEach(function(product, index) {
-//             const productInfo = document.createElement('div');
-//             productInfo.classList.add('product-info');
-//             productInfo.innerHTML = `
-//                 <p class="product-name">${product.name}</p>
-//                 <p class="product-description">${product.description}</p>
-//                 <p class="product-price">$${product.price}</p>
-//             `;
-//             recommendations.appendChild(productInfo);
-//         });
-//     });
-// }
-
-// script.js
-
-// script.js
-// script.js
-// script.js
-// script.js
-// script.js
 let productsData = [];
 
 function showSuggestions() {
